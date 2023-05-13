bfc710674aef377c63c8c0328d6c1901afdb1856
Jayesh <jayeshmishra121c@gmail.com>
2023-05-13 18:49:58 +0530

    Added function getSafeUrl in ts file

--- src/app/components/video-embed/video-embed.component.ts
+++ src/app/components/video-embed/video-embed.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
+import { Component, Input, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'video-embed',
@@ -9,9 +9,18 @@ import { DomSanitizer } from '@angular/platform-browser';
 export class VideoEmbedComponent {
   @Input() site: string = 'YouTube';
   @Input() key: string | null = null;
-
+  videoUrl: SafeResourceUrl = '';
   constructor(private sanitizer: DomSanitizer) {}
-
+  ngOnInit(): void {
+    switch (this.site) {
+      case 'YouTube':
+        this.videoUrl = this.getSafeUrl('https://www.youtube.com/embed/' + this.key);
+        break;
+      case 'Vimeo':
+        this.videoUrl = this.getSafeUrl('https://www.vimeo.com/embed/' + this.key);
+        break;
+    }
+  }
   getSafeUrl(url: string) {
     return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
